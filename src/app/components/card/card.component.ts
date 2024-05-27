9e4a6fdaf47b45d560d5901cba0f0ee6fe2d2ef5
felipesantos22 <felipevs2018@gmail.com>
2024-05-27 12:26:35 -0300

    insert filter to framework

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -12,14 +12,30 @@ import { CommonModule } from '@angular/common';
 })
 export class CardComponent implements OnInit {
   leng: Leng[] = [];
+  allLengs: Leng[] = [];
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.leng = this.apiService.getLengs();
+    this.allLengs = [...this.leng];
   }
 
-  sortCountriesByName(): void {
+  sortLeng(): void {
     this.leng.sort((a, b) => a.title.localeCompare(b.title));
+  }
+
+  filterFrameworks(): void {
+    this.leng = this.allLengs.filter((l) => l.category === 'Framework');
+  }
+
+  filterOO(): void {
+    this.leng = this.allLengs.filter(
+      (l) => l.category === 'Orientado a Objetos'
+    );
+  }
+
+  resetFilters(): void {
+    this.leng = [...this.allLengs];
   }
 }
--- src/app/data/leng.ts
+++ src/app/data/leng.ts
@@ -1,118 +1,114 @@
-import { Leng } from "../interfaces/leng";
+import { Leng } from '../interfaces/leng';
 
 const lenguages: Leng[] = [
-    {
-        id: 1,
-        title: "JavaScript",
-        description: "JavaScript é uma linguagem de programação versátil e de alto nível usada para desenvolvimento web, conhecida por seu modelo de I/O não bloqueante e orientado a eventos.",
-        category: "Desenvolvimento Web",
-        image: "https://i.pinimg.com/564x/d4/8d/dc/d48ddc557f7324e307f0dd135aa19019.jpg"
-    },
-    {
-        id: 2,
-        title: "Python",
-        description: "Python é uma linguagem de alto nível e interpretada, com ênfase na legibilidade e simplicidade, amplamente usada em ciência de dados, IA e desenvolvimento web.",
-        category: "Propósito Geral",
-        image: "https://i.pinimg.com/736x/71/40/77/714077561934f5da759e2fd1d7e1b994.jpg"
-    },
-    {
-        id: 3,
-        title: "Java",
-        description: "Java é uma linguagem de programação de alto nível, baseada em classes e orientada a objetos, projetada para ter o menor número possível de dependências de implementação.",
-        category: "Propósito Geral",
-        image: "https://i.pinimg.com/564x/00/b7/1d/00b71d49ba404e5d40898613a53eca54.jpg"
-    },
-    {
-        id: 4,
-        title: "C++",
-        description: "C++ é uma extensão da linguagem de programação C, conhecida por seu desempenho e usada em software de sistemas, desenvolvimento de jogos e simulações em tempo real.",
-        category: "Programação de Sistemas",
-        image: "https://i.pinimg.com/564x/51/c0/1f/51c01f3d91a44d481f619a65b0ce63c8.jpg"
-    },
-    {
-        id: 5,
-        title: "C#",
-        description: "C# é uma linguagem de programação moderna e orientada a objetos desenvolvida pela Microsoft, amplamente usada para criar aplicativos Windows e desenvolvimento de jogos com Unity.",
-        category: "Propósito Geral",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 6,
-        title: "Ruby",
-        description: "Ruby é uma linguagem de programação dinâmica e de código aberto, com foco na simplicidade e produtividade, conhecida por sua sintaxe elegante e fácil de ler e escrever.",
-        category: "Desenvolvimento Web",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 7,
-        title: "PHP",
-        description: "PHP é uma linguagem de script de uso geral popular, especialmente adequada para desenvolvimento web, embutida em HTML e executada no servidor.",
-        category: "Desenvolvimento Web",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 8,
-        title: "Swift",
-        description: "Swift é uma linguagem de programação poderosa e intuitiva para macOS, iOS, watchOS e tvOS, desenvolvida pela Apple para ser segura, rápida e interativa.",
-        category: "Desenvolvimento Mobile",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 9,
-        title: "Kotlin",
-        description: "Kotlin é uma linguagem de programação estaticamente tipada para aplicativos multiplataforma modernos, conhecida por ser concisa, expressiva e segura, e é oficialmente suportada para desenvolvimento Android.",
-        category: "Desenvolvimento Mobile",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 10,
-        title: "Go",
-        description: "Go, também conhecida como Golang, é uma linguagem de programação de código aberto projetada para simplicidade, confiabilidade e eficiência, frequentemente usada em aplicativos de nuvem e servidor.",
-        category: "Programação de Sistemas",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 11,
-        title: "R",
-        description: "R é uma linguagem de programação e um ambiente de software livre para computação estatística e gráficos, amplamente usada entre estatísticos e mineradores de dados.",
-        category: "Ciência de Dados",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 12,
-        title: "TypeScript",
-        description: "TypeScript é uma linguagem de programação fortemente tipada, orientada a objetos e compilada, desenvolvida e mantida pela Microsoft, um superset do JavaScript que adiciona tipos estáticos.",
-        category: "Desenvolvimento Web",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 13,
-        title: "Rust",
-        description: "Rust é uma linguagem de programação de sistemas que é rápida, previne falhas de segmentação e garante segurança de thread, frequentemente usada em aplicativos críticos de desempenho.",
-        category: "Programação de Sistemas",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 14,
-        title: "Scala",
-        description: "Scala é uma linguagem de tipagem estática forte que funde programação funcional e orientada a objetos em um pacote prático, executando na JVM e usada para aplicações de big data.",
-        category: "Propósito Geral",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 15,
-        title: "Perl",
-        description: "Perl é uma linguagem de programação de alto nível, uso geral, interpretada e dinâmica, conhecida por sua flexibilidade e capacidades de processamento de texto.",
-        category: "Propósito Geral",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    },
-    {
-        id: 16,
-        title: "MATLAB",
-        description: "MATLAB é uma linguagem de programação proprietária desenvolvida pela MathWorks, usada principalmente para computação numérica e amplamente usada em engenharia e pesquisa científica.",
-        category: "Ciência de Dados",
-        image: "https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg"
-    }
+  {
+    id: 1,
+    title: 'Java',
+    description:
+      'Java é uma linguagem de programação de alto nível, baseada em classes e orientada a objetos, projetada para ter o menor número possível de dependências de implementação.',
+    category: 'Orientado a Objetos',
+    image:
+      'https://i.pinimg.com/564x/00/b7/1d/00b71d49ba404e5d40898613a53eca54.jpg',
+  },
+  {
+    id: 2,
+    title: 'PHP',
+    description:
+      'PHP é uma linguagem de script de uso geral popular, especialmente adequada para desenvolvimento web, embutida em HTML e executada no servidor.',
+    category: 'Orientado a Objetos',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 3,
+    title: 'C#',
+    description:
+      'C# é uma linguagem de programação moderna e orientada a objetos desenvolvida pela Microsoft, amplamente usada para criar aplicativos Windows e desenvolvimento de jogos com Unity.',
+    category: 'Orientado a Objetos',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 4,
+    title: 'Kotlin',
+    description:
+      'Kotlin é uma linguagem de programação estaticamente tipada para aplicativos multiplataforma modernos, conhecida por ser concisa, expressiva e segura, e é oficialmente suportada para desenvolvimento Android.',
+    category: 'Orientado a Objetos',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 5,
+    title: 'JavaScript',
+    description:
+      'JavaScript é uma linguagem de programação versátil e de alto nível usada para desenvolvimento web, conhecida por seu modelo de I/O não bloqueante e orientado a eventos.',
+    category: 'Funcional',
+    image:
+      'https://i.pinimg.com/564x/d4/8d/dc/d48ddc557f7324e307f0dd135aa19019.jpg',
+  },
+  {
+    id: 6,
+    title: 'TypeScript',
+    description:
+      'TypeScript é uma linguagem de programação fortemente tipada, orientada a objetos e compilada, desenvolvida e mantida pela Microsoft, um superset do JavaScript que adiciona tipos estáticos.',
+    category: 'Orientado a Objetos',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 8,
+    title: 'Angular',
+    description:
+      'Angular é uma plataforma de aplicação web de código aberto liderada pela equipe Angular do Google e por uma comunidade de indivíduos e empresas.',
+    category: 'Framework',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 9,
+    title: 'Spring',
+    description:
+      'Spring é um framework open-source para a plataforma Java, usado para desenvolvimento de aplicações de grande porte com foco em produtividade, velocidade e simplicidade.',
+    category: 'Framework',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 10,
+    title: 'Next',
+    description:
+      'Next.js é um framework React que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para aplicações web.',
+    category: 'Framework',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 11,
+    title: 'Nest',
+    description:
+      'NestJS é um framework para construir aplicações Node.js eficientes e escaláveis, com suporte total para TypeScript, usando JavaScript progressivo.',
+    category: 'Framework',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 12,
+    title: 'Laravel',
+    description:
+      'Laravel é um framework PHP de código aberto, robusto e fácil de usar, que segue o padrão MVC (Model-View-Controller) e é usado para desenvolvimento de aplicações web.',
+    category: 'Framework',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
+  {
+    id: 13,
+    title: 'React',
+    description:
+      'React é uma biblioteca JavaScript de código aberto para criar interfaces de usuário, mantida pelo Facebook e uma comunidade de desenvolvedores individuais e empresas.',
+    category: 'Framework',
+    image:
+      'https://i.pinimg.com/564x/31/04/26/310426534f942e2db88bf6fd38549779.jpg',
+  },
 ];
 
 export default lenguages;
